85cfe03e012f93258cc00a3d51fbee9d40b3033a
swapmali <maliswapnil982@gmail.com>
2019-10-23 02:06:18 +0530

    More changes in part_3.py file

--- part_3.py
+++ part_3.py
@@ -1,10 +1,13 @@
 import os
 import time
-
+"""
+NOTE:   @ time of crawling, The domain link should be like http://cse.nitk.ac.in and not like http://cse.nitk.ac.in/
+TO DO: remove repeated lines in levels array
+"""
 
 fp = open("sitemap_links.txt", "r")
 site_links = []
-n = 5
+n = 10
 levels = [[] for x in range(n)]
 
 link = fp.readline()
@@ -38,12 +41,13 @@ for i in range(len(site_links)):
 set_new_cur_link = 1
 k = 1
 this_level_arr = []
-relation = [[[] for y in range(10)] for x in range(n)]
-print(relation)
+relation = [[[] for y in range(len(site_links))] for x in range(n)]
+
 # remaining iterations
 # k = level
 while k < n:
     # creating this level headers array
+    this_level_arr = []
     for j in range(len(site_links)):
         if site_links[j] == '':
             pass
@@ -69,6 +73,7 @@ while k < n:
                 pass
             else:
                 # remove current headers from the site_links and add relation from this headers to next level children
+                # e.g. for level 1 parent relation 1 contains its children (obv which lies in level 2)
                 part_of_site = site_links[i][:len(cur_link)]
                 if part_of_site == cur_link:
                     site_links[i] = site_links[i][len(cur_link) + 1:]
@@ -81,12 +86,15 @@ while k < n:
 
             set_new_cur_link = 1
 
-    this_level_arr = []
     # print(site_links)
     k += 1
 
 print(site_links)
-print(levels)
+print("-"*30 + "Levels" + "-"*30)
+for i in range(len(levels)):
+    print(i, levels[i])
 print()
+print("-"*30 + "Relations" + "-"*30)
 for i in range(len(relation)):
     print(i, relation[i])
+
